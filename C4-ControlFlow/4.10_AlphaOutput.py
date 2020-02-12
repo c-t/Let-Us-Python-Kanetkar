f4234cfd75438b43415423e1dee0c907a44a801c
Christ Toppo <christ.toppo@gmail.com>
2020-02-12 09:41:09 +0530

    Chapter 4 (Control Flow Instructions) Examples

---
+++ C4-ControlFlow/4.10_AlphaOutput.py
@@ -0,0 +1,6 @@
+for alpha in range(65,91):
+    print(chr(alpha),end=',')
+print()
+for alpha in range(122,96,-1):
+    print(chr(alpha),end=',')
+
---
+++ C4-ControlFlow/4.12_NumPosNeg0.py
@@ -0,0 +1,7 @@
+n=int(input('Enter a num: '))
+if n > 0:
+    flag = True
+    print(n*n)
+else:
+    flag = False
+    print(n*n*n)
