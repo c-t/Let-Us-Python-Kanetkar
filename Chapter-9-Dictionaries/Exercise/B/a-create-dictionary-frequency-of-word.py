8950e780c5c5d34d4acea4185a7e141a5b8d2b45
Christ Toppo <christ.toppo@gmail.com>
2020-05-06 12:28:50 +0530

    Add files via upload

---
+++ Chapter-9-Dictionaries/Exercise/B/a-create-dictionary-frequency-of-word.py
@@ -0,0 +1,5 @@
+"""
+WAP that reads a string from the keyboard and creates dictionary containing frequency of each character occurring in the string.
+Also print these occurrences in the form of a histogram
+"""
+string = input('Enter a string: ')
